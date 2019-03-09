45113744c24b53773477eb2c4f65ea5e07bd1bdb
haris-ramzan <haris.ramzan@blueeast.com>
2019-03-10 00:52:09 +0500

    fix: Build error

--- src/sampleCode.ts
+++ src/sampleCode.ts
@@ -1,4 +1,4 @@
-
+c;
 const Reactotron = require('reactotron-react-js').default;
 import { createPlugin, BlueBase, BootOptions } from '@bluebase/core';
 
@@ -7,16 +7,17 @@ export const ReactotronDebugger = createPlugin({
 	name: 'reactotron',
 	categories: ['debugger'],
 
-
 	defaultConfigs: {
 		'plugins.reactotron.useTrackGlobalErrors': false,
-		'plugins.reactotron.apisauce': false,
+		'plugins.reactotron.apisauce': false
 	},
 
 	filters: {
-
-		'bluebase.boot.end': (_bootOptions: BootOptions, _ctx: any, BB: BlueBase) => {
-
+		'bluebase.boot.end': (
+			_bootOptions: BootOptions,
+			_ctx: any,
+			BB: BlueBase
+		) => {
 			Reactotron.configure();
 			// .use(BB.Configs.getValue('plugins.reactotron.useTrackGlobalErrors'))
 			// .apisauce(BB.Configs.getValue('plugins.reactotron.apisauce'))
@@ -27,26 +28,30 @@ export const ReactotronDebugger = createPlugin({
 			Reactotron.log('hello rendering world');
 			BB.Logger.log('test====>');
 		},
-		'bluebase.plugins.initialize': (_bootOptions: BootOptions, _ctx: any, BB: BlueBase) => {
-
-			Reactotron
-				.configure()
+		'bluebase.plugins.initialize': (
+			_bootOptions: BootOptions,
+			_ctx: any,
+			BB: BlueBase
+		) => {
+			Reactotron.configure()
 				// .use(BB.Configs.getValue('plugins.reactotron.useTrackGlobalErrors'))
 				// .apisauce(BB.Configs.getValue('plugins.reactotron.apisauce'))
 				.connect();
 
 			// send data to logging provider here
 		},
-		'bluebase.logger.debug': (message: string, { params }: { params: any }, BB: BlueBase) => {
-
+		'bluebase.logger.debug': (
+			message: string,
+			{ params }: { params: any },
+			BB: BlueBase
+		) => {
 			Reactotron.log(message, params);
 
 			Reactotron.display({
 				message,
 				params,
 				BB
-			})
+			});
 		}
-	},
-
-})
+	}
+});
