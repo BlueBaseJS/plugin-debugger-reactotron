68ce18f9bfa7395b8509a28b89ccf1e2bafede87
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-10-08 21:49:48 +0500

    removed dist

--- dist/bluebase/common/bluebase.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    plugins: import("@bluebase/core/dist/registries/BlueBaseModuleRegistry").BlueBaseModuleRegistryInputItem<Partial<import("@bluebase/core").PluginValue>>[];
-    configs: {
-        'plugins.reactotron.configure': boolean;
-        'plugins.reactotron.connect': boolean;
-        'plugins.reactotron.apisauce': boolean;
-    };
-};
-export default _default;
--- dist/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const ReactotronDebugger: import("@bluebase/core/dist/registries/BlueBaseModuleRegistry").BlueBaseModuleRegistryInputItem<Partial<import("@bluebase/core").PluginValue>>;
